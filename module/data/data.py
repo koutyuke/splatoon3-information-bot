e2de9ad1c6eed280ca4f2ccc2902bc440f5d59ce
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-11-22 10:30:03 +0900

    fix: x font size

--- module/data/data.py
+++ module/data/data.py
@@ -2,6 +2,6 @@ propsData = {
     "regular": {"battleType": "レギュラーマッチ", "color": (0, 255, 0)},
     "open": {"battleType": "バンカラマッチ(オープン)", "color": (255, 165, 0)},
     "challenge": {"battleType": "バンカラマッチ(チャレンジ)", "color": (255, 165, 0)},
-    "x": {"battleType": "xマッチ", "color": (255, 165, 0)},
+    "x": {"battleType": "Xマッチ", "color": (255, 165, 0)},
     "league": {"battleType": "リーグマッチ", "color": (255, 25, 147)},
 }
