65b5344cd58e420da53ca63aa22538ff7494d589
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-11-22 09:52:06 +0900

    add: data

---
+++ module/data/data.py
@@ -0,0 +1,7 @@
+propsData = {
+    "regular": {"battleType": "レギュラーマッチ", "color": (0, 255, 0)},
+    "open": {"battleType": "バンカラマッチ(オープン)", "color": (255, 165, 0)},
+    "challenge": {"battleType": "バンカラマッチ(チャレンジ)", "color": (255, 165, 0)},
+    "x": {"battleType": "xマッチ", "color": (255, 165, 0)},
+    "league": {"battleType": "リーグマッチ", "color": (255, 25, 147)},
+}
