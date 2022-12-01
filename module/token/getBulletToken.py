0d3df1dca8a353b2152c10d5df9a7220c6ddb6fc
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-12-01 10:51:35 +0900

    fix: change x-web-view-ver

--- module/token/getBulletToken.py
+++ module/token/getBulletToken.py
@@ -1,7 +1,10 @@
 import requests
+import os
+from dotenv import load_dotenv
 
 
 def getBulletToken(xGamewebtoken: str):
+    load_dotenv()
     url = "https://api.lp1.av5ja.srv.nintendo.net/api/bullet_tokens"
 
     headers = {
@@ -9,10 +12,13 @@ def getBulletToken(xGamewebtoken: str):
         "Connection": "keep-alive",
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "ja-JP",
-        "X-Web-View-Ver": "1.0.0-42f70e51",
+        "X-Web-View-Ver": os.environ["WEB_VIEW_VER"],
         "X-GameWebToken": xGamewebtoken,
     }
 
     data = requests.post(url=url, headers=headers)
+
+    print(xGamewebtoken)
+    print(data)
 
     return data.json()
